5fdbfb2cdbcab429f8a8054defd6b5064dfd2ff5
Daniel <danielsavina862@gmail.com>
2025-06-11 22:54:21 -0500

    cambio1

--- BibliotecaGrupo2/src/modelos/usuarios/Bibliotecario.java
+++ BibliotecaGrupo2/src/modelos/usuarios/Bibliotecario.java
@@ -1,5 +1,78 @@
 package modelos.usuarios;
-
-public class Bibliotecario {
-    
+import java.util.List;
+public class Bibliotecario extends Usuario {
+    private List<String> permisos;
+    public Bibliotecario(String idUsuario, String nombreUsuario, String contrasena,
+                    String nombreCompleto, String email, List<String> rol, List<String> permisos) {
+        super(idUsuario, nombreUsuario, contrasena, nombreCompleto, email, rol);
+        this.permisos = permisos;
+    }
+    @Override
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+    @Override
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    @Override
+    public String getContrasena() {
+        return contrasena;
+    }
+    @Override
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+    @Override
+    public String getEmail() {
+        return email;
+    }
+    @Override
+    public List<String> getRol() {
+        return rol;
+    }
+    @Override
+    public boolean isActivo() {
+        return activo;
+    }
+    @Override
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    @Override
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    @Override
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    @Override
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Override
+    public void setRol(List<String> rol) {
+        this.rol = rol;
+    }
+    @Override
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    public boolean tienePermiso(String permiso) {
+        return permisos != null && permisos.contains(permiso);
+    }
+    public boolean autenticar(String contrasena) {
+        return this.contrasena != null && this.contrasena.equals(contrasena);
+    }
+    public List<String> getPermisos() {
+        return permisos;
+    }
+    public void setPermisos(List<String> permisos) {
+        this.permisos = permisos;
+    }
 }
--- BibliotecaGrupo2/src/modelos/usuarios/Usuario.java
+++ BibliotecaGrupo2/src/modelos/usuarios/Usuario.java
@@ -1,5 +1,35 @@
 package modelos.usuarios;
-
-public class Usuario {
-    
+import java.util.List;
+public abstract class Usuario {
+    protected String idUsuario;
+    protected String nombreUsuario;
+    protected String contrasena;
+    protected String nombreCompleto;
+    protected String email;
+    protected List<String> rol;  
+    protected boolean activo;
+    public Usuario(String idUsuario, String nombreUsuario, String contrasena, 
+                   String nombreCompleto, String email, List<String> rol) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.nombreCompleto = nombreCompleto;
+        this.email = email;
+        this.rol = rol;
+        this.activo = true; 
+    }
+    public abstract String getIdUsuario();
+    public abstract String getNombreUsuario();
+    public abstract String getContrasena();
+    public abstract String getNombreCompleto();
+    public abstract String getEmail();
+    public abstract List<String> getRol();
+    public abstract boolean isActivo();
+    public abstract void setIdUsuario(String idUsuario);
+    public abstract void setNombreUsuario(String nombreUsuario);
+    public abstract void setContrasena(String contrasena);
+    public abstract void setNombreCompleto(String nombreCompleto);
+    public abstract void setEmail(String email);
+    public abstract void setRol(List<String> rol);
+    public abstract void setActivo(boolean activo);
 }
