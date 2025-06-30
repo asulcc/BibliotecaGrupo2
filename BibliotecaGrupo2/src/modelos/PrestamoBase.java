0da086f52eacd6486acffdfbfda4b628de36cf58
PatrickJoveV <dragonzahid88@gmail.com>
2025-06-30 00:22:35 -0500

    Update PrestamoBase.java

--- BibliotecaGrupo2/src/modelos/PrestamoBase.java
+++ BibliotecaGrupo2/src/modelos/PrestamoBase.java
@@ -1,5 +1,38 @@
-package modelos;
+import java.time.LocalDate;
 
-public class PrestamoBase {
-    
+public abstract class PrestamoBase {
+    protected int id;
+    protected Usuario usuario;
+    protected LocalDate fechaPrestamo;
+    protected LocalDate fechaDevolucionPrevista;
+    protected LocalDate fechaDevolucionReal;
+    protected boolean extendido;
+//constructo
+    public PrestamoBase(int id, Usuario usuario, LocalDate fechaPrestamo,
+                        LocalDate fechaDevolucionPrevista, LocalDate fechaDevolucionReal) {
+        this.id = id;
+        this.usuario = usuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucionPrevista = fechaDevolucionPrevista;
+        this.fechaDevolucionReal = fechaDevolucionReal;
+        this.extendido = false; //inicia como false pq aun no se sabe si el tiempo se va a extender
+    }
+//geters
+    public int getId() { return id; }
+    public Usuario getUsuario() { return usuario; }
+    public LocalDate getFechaPrestamo() { return fechaPrestamo; }
+    public LocalDate getFechaDevolucionPrevista() { return fechaDevolucionPrevista; }
+    public LocalDate getFechaDevolucionReal() { return fechaDevolucionReal; }
+    public boolean isExtendido() { return extendido; }
+//seters
+    public void setId(int id) { this.id = id; }
+    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+    public void setFechaPrestamo(LocalDate fechaPrestamo) { this.fechaPrestamo = fechaPrestamo; }
+    public void setFechaDevolucionPrevista(LocalDate fechaDevolucionPrevista) { this.fechaDevolucionPrevista = fechaDevolucionPrevista; }
+    public void setFechaDevolucionReal(LocalDate fechaDevolucionReal) { this.fechaDevolucionReal = fechaDevolucionReal; }
+    public void setExtendido(boolean extendido) { this.extendido = extendido; }
+
+    public abstract long calcularDiasRetraso();
+    public abstract boolean estaVencido();
 }
+
