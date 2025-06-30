44bb18dad82aa4731a1ecb9cd48b8e6b74929e17
PatrickJoveV <dragonzahid88@gmail.com>
2025-06-29 22:36:31 -0500

    Update Usuario.java

--- BibliotecaGrupo2/src/modelos/usuarios/Usuario.java
+++ BibliotecaGrupo2/src/modelos/usuarios/Usuario.java
@@ -1,5 +1,64 @@
 package modelos.usuarios;
 
-public class Usuario {
-    
+public abstract class Usuario {
+    protected int id;
+    protected String nombreUsuario;
+    protected String contrasena;
+    protected String nombreCompleto;
+    protected String email;
+    protected Rol rol;
+
+    public Usuario(int id, String nombreUsuario, String contrasena, String nombreCompleto, String email, Rol rol) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.nombreCompleto = nombreCompleto;
+        this.email = email;
+        this.rol = rol;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    // Método abstracto que debe ser implementado por las subclases
+    public abstract void mostrarInformacion();
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Usuario: " + nombreUsuario + ", Rol: " + rol.name() + ", Nombre: " + nombreCompleto;
+    }
 }
