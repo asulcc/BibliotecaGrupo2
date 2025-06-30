4b3b822437b564bb67013b462c6b63094d087939
PatrickJoveV <dragonzahid88@gmail.com>
2025-06-29 23:41:30 -0500

    Update PrestamoBase.java

--- BibliotecaGrupo2/src/modelos/PrestamoBase.java
+++ BibliotecaGrupo2/src/modelos/PrestamoBase.java
@@ -1,49 +1,5 @@
 package modelos;
 
-import modelos.usuarios.Usuario;
-import java.time.LocalDate;
-
-public abstract class PrestamoBase {
-    protected int id;
-    protected Usuario usuario;
-    protected LocalDate fechaInicio;
-    protected LocalDate fechaFinPrevista;
-    protected boolean completado; // Indica si el préstamo/reserva ha sido finalizado/liberado
-
-    public PrestamoBase(int id, Usuario usuario, LocalDate fechaInicio, LocalDate fechaFinPrevista) {
-        this.id = id;
-        this.usuario = usuario;
-        this.fechaInicio = fechaInicio;
-        this.fechaFinPrevista = fechaFinPrevista;
-        this.completado = false; // Por defecto, no completado al crearse
-    }
-
-    // Getters
-    public int getId() {
-        return id;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public LocalDate getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public LocalDate getFechaFinPrevista() {
-        return fechaFinPrevista;
-    }
-
-    public boolean isCompletado() {
-        return completado;
-    }
-
-    // Setter
-    public void setCompletado(boolean completado) {
-        this.completado = completado;
-    }
-
-    // Método abstracto para verificar si el préstamo/reserva está vencido.
-    public abstract boolean estaVencido();
+public class PrestamoBase {
+    
 }
