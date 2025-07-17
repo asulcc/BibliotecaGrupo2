96fce2af473d4a1878141762aa755f77667e49b5
Daniel <danielsavina862@gmail.com>
2025-07-16 22:45:35 -0500

    cambio para optimizar

--- BibliotecaGrupo2/src/modelos/usuarios/Bibliotecario.java
+++ BibliotecaGrupo2/src/modelos/usuarios/Bibliotecario.java
@@ -7,59 +7,45 @@ public class Bibliotecario extends Usuario {
         super(idUsuario, nombreUsuario, contrasena, nombreCompleto, email, rol);
         this.permisos = permisos;
     }
-    @Override
     public String getIdUsuario() {
         return idUsuario;
     }
-    @Override
     public String getNombreUsuario() {
         return nombreUsuario;
     }
-    @Override
     public String getContrasena() {
         return contrasena;
     }
-    @Override
     public String getNombreCompleto() {
         return nombreCompleto;
     }
-    @Override
     public String getEmail() {
         return email;
     }
-    @Override
     public List<String> getRol() {
         return rol;
     }
-    @Override
     public boolean isActivo() {
         return activo;
     }
-    @Override
     public void setIdUsuario(String idUsuario) {
         this.idUsuario = idUsuario;
     }
-    @Override
     public void setNombreUsuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
     }
-    @Override
     public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
-    @Override
     public void setNombreCompleto(String nombreCompleto) {
         this.nombreCompleto = nombreCompleto;
     }
-    @Override
     public void setEmail(String email) {
         this.email = email;
     }
-    @Override
     public void setRol(List<String> rol) {
         this.rol = rol;
     }
-    @Override
     public void setActivo(boolean activo) {
         this.activo = activo;
     }
